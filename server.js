08116627745839b2f9d57b08f337bee3b21f5991
Gerard Eklu <gerard_eklu@homedepot.com>
2020-02-17 16:47:08 -0500

    Adding two numbers from the server and outputting it to the ui using body parser

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen("3000", () => {
+app.post("/", (req, res) => {
+  let numberOne = Number(req.body.numberOne);
+
+  let numberTwo = Number(req.body.numberTwo);
+  let result = numberOne + numberTwo;
+  res.status(200).send(result.toString());
+});
+
+app.listen(3000, () => {
   console.log("App started on 3000");
 });
